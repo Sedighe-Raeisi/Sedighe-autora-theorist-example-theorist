e7c78d971d219abc964a92a61810060b50c0fbc2
Younes Strittmatter <younes_strittmatter@brown.edu>
2024-07-30 14:43:20 +0000

    initial commit

---
+++ src/autora/theorist/example_theorist/__init__.py
@@ -0,0 +1,39 @@
+"""
+Example Theorist
+"""
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator
+
+
+
+
+class ExampleRegressor(BaseEstimator):
+    """
+    Examples:
+        >>> regressor = ExampleRegressor()
+        >>> X = np.array([1,2,3])
+        >>> y = np.array([1,3,3])
+        >>> regressor.fit(X,y)
+        >>> regressor.predict(X)
+        np.array([1,3,3])
+
+
+    """
+
+    def __init__(self):
+        pass
+
+    def fit(self,
+            conditions: Union[pd.DataFrame, np.ndarray],
+            observations: Union[pd.DataFrame, np.ndarray]):
+            """
+
+            """
+        pass
+
+    def predict(self,
+                conditions: Union[pd.DataFrame, np.ndarray]) -> Union[pd.DataFrame, np.ndarray]:
+        pass
